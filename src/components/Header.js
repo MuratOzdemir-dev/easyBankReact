55573b36d579111cd8bdd9a64b7cc0c12738ac6e
AinTisar <mrtozdmr92@gmail.com>
2021-03-04 20:28:19 +0300

    First İnit.

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Header() {
+  return(
+    <header>
+      <div className="header__logo"></div>
+      <div className="header__desktop-menu"></div>
+      <div className="header__mobile-menu"></div>
+      <div className="header__hamburger"></div>
+      <div className="header__overlay"></div>
+    </header>
+  )
+}
+
+export default Header;
